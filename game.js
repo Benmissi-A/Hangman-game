55637dc500ceba3646cb7eb7f3eee6ca20be850f
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-02 16:59:08 +0200

    some functions

---
+++ game.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const { randomInt } = require('crypto')
+const words = fs.readFileSync('dict.txt','utf-8').toUpperCase().split('\n')
+
+
+//generation de mots aleatoire
+const randomWord = (array) => {
+  let n = randomInt(0, array.length)
+  return words[n]
+}
+
+//generation de mot secret
+//let word = randomWord(words)
+let word = randomWord(words)
+let secret = '_ '.repeat(word.length).trim()
+
+
+
+//on devine les lettres
+const guess = (word,secret,char) => {
+  if(word.includes(char)){
+    tmp =''
+    for(let i = 0 ; i < word.length;++i){
+     word[i]===char ? tmp += char+' ' : tmp +='_ '
+    }
+    return [tmp,true]
+  }else{
+  return [secret,false]
+  } 
+}
+
+
+
+[secret,isSuccess] = guess(word , secret,'A')
+// une partie
+
+exports.guess = guess
+exports.randomWord = randomWord
--- hang-man.js
+++ hang-man.js
@@ -3,15 +3,22 @@ const readlineSync = require('readline-sync')
 const chalk = require('chalk')
 const header = fs.readFileSync('header.txt','utf-8')
 console.log(chalk.yellow(header))
+const {guess,randomWord} = require('game')
 
 // menu principal du jeu
 
-// let options = ['Commencer une partie','Meilleurs scores']
-// let index = readlineSync.keyInSelect(options, 'selectionner une option')
-// switch(index) {
-//   case 0 :
-//     console.log('commencer la partie')
-//     break
-//   case 1 :
-//     console.log('voire les meilleurs scores')
-// }
+let options = ['Commencer une partie','Meilleurs scores']
+let index = readlineSync.keyInSelect(options, 'selectionner une option')
+switch(index) {
+  case 0 :
+    console.log('commencer la partie')
+
+    break
+  case 1 :
+    console.log('voire les meilleurs scores')
+}
+
+// marche a suivre 
+//  le menu principal propose de lancer une partie
+
+//  
