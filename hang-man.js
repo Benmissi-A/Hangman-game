0784ec7d279b94069643b5f785687a0296b95b5e
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-02 13:59:22 +0200

    menu principal

---
+++ hang-man.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const readlineSync = require('readline-sync')
+const chalk = require('chalk')
+const header = fs.readFileSync('header.txt','utf-8')
+console.log(chalk.yellow(header))
+
+// menu principal du jeu
+
+let options = ['Commencer une partie','Meilleurs scores','Quitter le jeu']
+let index = readlineSync.keyInSelect(options, 'selectionner une option')
+switch(index) {
+  case 0 :
+    console.log('commencer la partie')
+    break
+  case 1 :
+    console.log('voire les meilleurs scores')
+  case 2 :
+    console.log('quitter le jeu')
+    process.exit
+}
